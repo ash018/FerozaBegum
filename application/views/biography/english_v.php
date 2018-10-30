1c8f317e1beaeab82511cd8c1e29b571fd381f61
MIS-IA-Akash <smakash@aci-bd.local>
2018-10-30 17:24:51 +0600

    biography Changed 30/10/18

--- application/views/biography/english_v.php
+++ application/views/biography/english_v.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster&effect=brick-sign">
 <div class="f_top_margin_30"></div>
 <div class="col-md-12 line"></div>
 <div class="container-fluid" style="background-color: #8e8e98">
@@ -9,61 +10,31 @@
                        <img src="<?php echo base_url()?>asset/img/Young_Firoza_Begum_1955.jpg" class="img-responsive" alt="Firoza Begum"> 
                     </div>
                     <div class="col-md-10 f_body_text_right">
-                        <h4>Biography</h4>
-                        <p>Nazrul Sangeet legend Firoza Begum was born in a Muslim family in Faridpur, Bangladesh on 28 July 1930 to the<br>Zamindars of Ratail Ghonaparha. She became drawn to music in her childhood. She started her career in 1940s.<br>
-							She first sang in All India Radio while studying in sixth grade. She met poet Kazi Nazrul Islam at the age of 10. In 1942,<br> she recorded her first Islamic song by the gramophone record company HMV in 78 rpm disk format. Since then, 12 LP, 4<br> EP, 6 CD and more than 20 audio cassette records have been released. She lived in Kolkata from 1954 until she moved<br>to Dhaka in 1967. 
+                        <h3>Feroza Begum</h3>
+                        <h5>Short Biography</h5>
+                        <p>Nazrul Sangeet legend Feroza Begum was born on 28 July in 1926 as the third child of a Noble family in Faridpur, Bangladesh. Eminent lawyer Khan Bahadur Mohammad Ismail is her father and Begum Kowkabunnesa is her mother.     
 						</p>
-						<p>	In 1956, Firoza Begum was married to Kamal Das Gupta, a singer, composer and lyricist. Kamal died on 20 July 1974. Her<br> sons Hamin Ahmed and Shafin Ahmed are also musicians. They are currently members of the rock band Miles.<br>
-							Firoza Begum died on 9 September 2014 around 8:28 pm in Apollo Hospital, Dhaka at the age of 84 due to heart and<br> kidney problems.
-						</p>	
-						<p><pre>Born  28 July 1930<br>Faridpur, British Raj (now Bangladesh).</pre></p>
+						<p>	Feroza Begum formally started learning music at the age of 13 from Chitta Roy in Kolkata. After one year, Feroza Begum started her training in music under the direct supervision of the legendary music director of the Indian subcontinent, Kamal Dasgupta. When Feroza Begum was only 14, the famous Columbia Records in Kolkata published her first music album. At that time, Feroza Begum was regularly performing at the Kolkata Radio. Her radio performances encompassed a wide range of music genres such as Islamic Song, Geet, Ghazal, Bhajan, Modern Bengali Song, and Nazrul Sangeet. The world renowned record company His Master’s Voice then started publishing one after another many of her gramophone records in Bengali, Hindi, and Urdu. Among them, Feroza Begum’s renditions of the most popular songs were “Amar Moner Sagor Kinare”, “Kabhu Pai Kabhu Pai Na”, “Tumi Jodi Chaho More”, “Ekti Heridoy Gaan Geye Jaay”, “Tum Bhulaye Na Gaye”, “Kyon Tum Se Mujh Ko Pyar Hay”,  “Kab Tak Yeh Pareshani”, “Thehro Thehro Zara”.   
+						</p>
+                                                <p>
+                                                    By then, Feroza Begum had started her training in music directly from Kazi Nazrul Islam. In spite of being a very famous vocal artist at that time for her renditions of Modern Bengali Songs and Urdu Songs, Feroza Begum was drawn to Nazrul Sangeet and started concentrating entirely on Nazrul’s songs. Through rigorous practice, she could adapt an original style of singing that brought Nazrul Sangeet near to the hearts of the common people. She took her struggle in popularizing Nazrul’s songs to the heights of a movement in music. Through this movement, Nazrul’s modern songs and Nazrul Geeti, previously ignored by the general audiences, appeared as a new genre of Nazrul Sangeet. Feroza Begum became known by then as the “rebel disciple of the rebel poet”.          
+                                                </p>
+                                                <p>
+                                                    At that time, all the renowned vocal artists in Kolkata were recording Nazrul Sangeet under the direction and supervision of Feroza Begum. She herself has rendered and recorded over seven hundred songs. The music legend has 36 Long Play records to her credit and has made 150 stage performances at the Mahajati Sadan and the Rabindra Sadan in Kolkata.  
+                                                </p>
+                                                <p>
+                                                    The Government of Bangladesh has honoured Feroza Begum with ‘Independence Award’ in 1979. The Government of West Bengal in India honoured Feroza Begum with ‘Banga Samman Award’ in 2012, with the title of 'Mahaguru' in 2013 for her lifelong contribution in music, and the highest award in West Bengal ‘Banga Bibhushan’ in music in 2015. Besides,  Feroza Begum has received numerous awards throughout her 78 years of music career; ‘Netaji Subhas Chandrav Sammanona’, ‘Satyajit Ray Award’ (1992), D.Litt degree from the University of Burdwan, “Golden Disk” award from the famous Japanese record company CBS are to name a few. Here to mention, the “Golden Disk” award was presented to Feroza Begum as her rendition of Nazrul Sangeet “Shawana Raate Jadi” sold one million copies in one month.
+                                                </p>
+                                                <p>
+                                                    A true artist does not find success in awards and accolades. She wishes to reign over people’s hearts. Feroza Begum has acquired permanent space in the hearts of millions of her fans. After her death, at a programme, an ardent fan of Feroza Begum had once said that after the death of Kazi Nazrul Islam, everyone thought that if not the poet is here but Feroza Begum is here; but when Feroza Begum had died it seemed that Kazi Nazrul Islam has finally passed away. Every conscious moment of the 88 years of the music legend’s life shines bright with her struggle, her quest for excellence in music.  She had dedicated her life to collect, promote, and present Nazrul Sangeet to a wider audience. Feroza Begum has seen its success in her lifetime. Her quest for music made her a living legend.      
+                                                </p>
+                                                <p>
+                                                    Feroza Begum has left us forever and in tears on the 9th of September in 2014. The golden era of Nazrul Sangeet has also concluded with the end of her life.   
+                                                </p>
+                                                <p>
+                                                    Every year, the University of Dhaka honours a deserving national level vocal artist with ‘Feroza Begum Gold Medal’. Besides, the top honours student obtaining the higest CGPA in Bachelors of Arts program at the universirty receives a similar gold medal for academic excellance at the convocation ceremony.         
+                                                </p>
 						
-                         
-						
-						<table class='margintop20'>
-							<tr> 
-								 <th colspan="2">Died  9 September 2014 (aged 84)</th> 
-							</tr> 
-							
-							<tr>
-								<td>Nationality</td>
-								<td>Bangladeshi</td>
-							</tr>
-							
-							<tr>
-								<td>Occupation</td>
-								<td>Singer</td>
-							</tr>
-							
-							<tr>
-								<td>Years active &nbsp;</td>
-								<td>1940-2014</td>
-							</tr>
-							
-						</table>
-						      
-						       
-						<br>Spouse(s)  Kamal Das Gupta (m. 1955; died 1974)<br><br>Children  Tahsin ahmed<br>Hamin Ahmed<br>Shafin Ahmed<br><br>Parents   Khan Bahadur Mohammad Ismail<br>Begum Kowkabunnesa </pre></p>
-						
-						<p style = "font-family:Arial;">Awards</p>
-							<ul style = "margin-left: -24px;">
-							
-								<li>Independence Day Award (1979) </li>
-								<li>Netaji Shuvash Chandra Award</li>
-								<li>Satyajit Ray Award</li>
-								<li>Nasiruddin Gold Medal</li>
-								<li>Bangladesh Shilpakala Academy Gold Medal</li>
-								<li>Best Nazrul Sangeet Singer Award</li>
-								<li>Nazrul Academy Award</li>
-								<li>Hurulia Gold MEdal</li>
-								<li>D.Lit from Bordwan University</li>
-								<li>Gold Disk from CBS, Japan </li>
-								<li>Sheltech Award (2000)</li>
-								<li>Lifetime honorary award from Meril-Prothom Alo (2011)</li>
-								<li>Bongo Shomman from Mamata Banerjee(2012)</li>
-								<li>Banga Bibhushon Award West Bengal (2015)</li>
-								
-							</ul>
 						
                     </div>
                 </div>
